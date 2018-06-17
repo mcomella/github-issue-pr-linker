f9045a287891da5d853562b03c165f4ec50fc993
Michael Comella <michael.l.comella@gmail.com>
2018-06-17 08:45:41 -0700

    Closes #1: insert issue numbers on PR page.
    This add-on is now functionally equivalent to github-issue-hoister.

--- src/ts/github_page_pr.ts
+++ src/ts/github_page_pr.ts
@@ -8,6 +8,37 @@ namespace GithubPagePR {
     }
 
     export function inject() {
-        Log.l('github PR');
+        const {repo, owner} = getRepoOwnerIssueNum(window.location);
+
+        const prTitleNode = document.getElementsByClassName('js-issue-title')[0] as HTMLSpanElement;
+        const prTitle = prTitleNode.innerText;
+        const issues = GithubParser.getIssueNumsFromTitle(prTitle);
+        if (issues.length > 0) {
+            addToDOM(owner, repo, issues);
+        }
+    }
+
+    function addToDOM(owner: string, repo: string, issueNumbers: number[]) {
+        const container = GithubDOM.newContainerNode();
+        const titleNode = GithubDOM.newTitleNode('Issues referenced in this PR title:');
+        container.appendChild(titleNode);
+        const listNode = GithubDOM.newListNode();
+        container.appendChild(listNode);
+
+        const newNodes = Array.from(issueNumbers).map(issueNum =>
+                GithubDOM.createNodeForIssue(owner, repo, GithubPageType.ISSUE, issueNum));
+        newNodes.forEach(node => listNode.appendChild(node));
+
+        GithubDOM.insertAboveConversation(container);
+    }
+
+    function getRepoOwnerIssueNum(url: Location) {
+        const maybeMatch = RE_URL.exec(url.toString());
+        if (maybeMatch == null) { throw new Error('Regex doesn\'t match url'); }
+        return {
+            owner: maybeMatch[1],
+            repo: maybeMatch[2],
+            issueNumber: parseInt(maybeMatch[3]),
+        }
     }
 }
